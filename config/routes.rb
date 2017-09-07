a96016ba277713d40f3bf9aedd33f98070773750
musicprogram <juan-o-henao@hotmail.es>
2017-09-07 13:25:52 -0500

    vistas para registrados y no registrados, gem haml

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'welcome#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  
+	authenticated :user do 
+		root 'welcome#index'
+	end
+	
+	unauthenticated :user do 
+		devise_scope :user do
+			root 'welcome#unregistered'
+		end
+	end
+
+
 end
